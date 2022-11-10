bf269ba995f9be2823b38e4b2a0758ada53f0c53
shuningliu <shuningliu@ucsb.edu>
2022-11-09 23:38:47 -0800

    add table and change place holder

--- frontend/src/main/pages/MenuItems/MenuItemsIndexPage.js
+++ frontend/src/main/pages/MenuItems/MenuItemsIndexPage.js
@@ -1,13 +1,28 @@
-import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend'; // use prefix indicates a React Hook
 
-export default function MenuItemIndexPage() {
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import MenuItemsTable from 'main/components/MenuItems/MenuItemsTable';
+import { useCurrentUser } from 'main/utils/currentUser' // use prefix indicates a React Hook
+
+export default function MenuItemsIndexPage() {
+
+  const currentUser = useCurrentUser();
+
+  const { data: menuItem, error: _error, status: _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      ["/api/ucsbdinningcommonsmenu/all"],
+            // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
+            { method: "GET", url: "/api/ucsbdinningcommonsmenu/all" },
+      []
+    );
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Todos</h1>
-        <p>
-          This is where the index page will go
-        </p>
+        <h1>MenuItems</h1>
+        <MenuItemsTable menuItem={menuItem} currentUser={currentUser} />
       </div>
     </BasicLayout>
   )

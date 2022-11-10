56a32db85707688270b056b953ba178daf880c9a
Yu <yu_yao@ucsb.edu>
2022-11-10 10:18:52 -0800

    <YY - added placeholder for organization>

--- frontend/src/main/pages/organizations/OrganizationsIndexPage.js
+++ frontend/src/main/pages/organizations/OrganizationsIndexPage.js
@@ -1,13 +1,13 @@
-import React from 'react'
-
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 
-export default function OrganizationIndexPage() {
+export default function OrganizationsIndexPage() {
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Help Requests</h1>
-        <input type="text" placeholder="Organization placeholder text" />
+        <h1>UCSB Organizations</h1>
+        <p>
+          This is where the index page will go
+        </p>
       </div>
     </BasicLayout>
   )

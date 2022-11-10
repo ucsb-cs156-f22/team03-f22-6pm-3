cfdea7d0a5028c680db7914e5a1a1c6be3916860
Yu <yu_yao@ucsb.edu>
2022-11-10 12:31:54 -0800

    <Add storybook of organization table>

--- frontend/src/main/pages/organizations/OrganizationsIndexPage.js
+++ frontend/src/main/pages/organizations/OrganizationsIndexPage.js
@@ -1,13 +1,30 @@
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend'; // use prefix indicates a React Hook
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+//import UCSBDatesTable from 'main/components/UCSBDates/UCSBDatesTable';
+import _OrganizationsTable from 'main/components/Organizations/OrganizationsTable';
+import { useCurrentUser } from 'main/utils/currentUser' // use prefix indicates a React Hook
 
 export default function OrganizationsIndexPage() {
+
+  const _currentUser = useCurrentUser();
+
+  
+  const { data: _organizations, error: _error, status: _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      ["/api/ucsborganization/all"],
+            // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
+           { method: "GET", url: "/api/ucsborganization/all" },
+      []
+    );
+
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>UCSB Organizations</h1>
-        <p>
-          This is where the index page will go
-        </p>
+        {/*<OrganizationsTable dates={organizations} currentUser={currentUser} />*/}
       </div>
     </BasicLayout>
   )

049e8231e0146cb9b5fc2735cf96524c4e1e7bb6
Sergio Colis <sergioacolis@Sergios-MacBook-Pro.local>
2022-11-14 18:03:38 -0800

    SC - Implemented HelpRequestTable component to Storybooks && Implemented Tests

--- frontend/src/stories/components/HelpRequests/HelpRequestForm.stories.js
+++ frontend/src/stories/components/HelpRequests/HelpRequestForm.stories.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-import HelpRequestsForm from "main/components/HelpRequests/HelpRequestsForm"
+import HelpRequestForm from "main/components/HelpRequests/HelpRequestForm"
 import { helpRequestsFixtures } from 'fixtures/helpRequestsFixtures';
 
 export default {
-    title: 'components/HelpRequests/HelpRequestsForm',
-    component: HelpRequestsForm
+    title: 'components/HelpRequests/HelpRequestForm',
+    component: HelpRequestForm
 };
 
 
 const Template = (args) => {
     return (
-        <HelpRequestsForm {...args} />
+        <HelpRequestForm {...args} />
     )
 };
 
@@ -19,13 +19,13 @@ export const Default = Template.bind({});
 
 Default.args = {
     buttonLabel: "Create",
-    submitAction: (data) => { console.log('Create was clicked, parameter to submitAction=',data); }
+    submitAction: () => { console.log('Submit was clicked'); }
 };
 
 export const Show = Template.bind({});
 
 Show.args = {
-    initialCommons: helpRequestsFixtures.oneHelpRequest,
-    buttonLabel: "Update",
-    submitAction: (data) => { console.log('Update was clicked, parameter to submitAction=',data); }
+    initialRequests: helpRequestsFixtures.oneHelpRequest,
+    buttonLabel: "",
+    submitAction: () => { }
 };
--- frontend/src/stories/components/HelpRequests/HelpRequestTable.stories.js
+++ frontend/src/stories/components/HelpRequests/HelpRequestTable.stories.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-import HelpRequestsTable from "main/components/HelpRequests/HelpRequestsTable";
+import HelpRequestsTable from "main/components/HelpRequests/HelpRequestTable";
 import { helpRequestsFixtures } from 'fixtures/helpRequestsFixtures';
-import { currentUserFixtures } from 'fixtures/currentUserFixtures';
 
 export default {
-    title: 'components/HelpRequests/HelpRequestsTable',
+    title: 'components/HelpRequests/HelpRequestTable',
     component: HelpRequestsTable
 };
 
@@ -18,19 +17,13 @@ const Template = (args) => {
 export const Empty = Template.bind({});
 
 Empty.args = {
-    helpRequests: []
+    requests: []
 };
 
 export const ThreeDates = Template.bind({});
 
 ThreeDates.args = {
-    helpRequests: helpRequestsFixtures.threeHelpRequests
+    requests: helpRequestsFixtures.threeHelpRequests
 };
 
-export const ThreeDatesAsAdmin = Template.bind({});
-
-ThreeDatesAsAdmin.args = {
-    helpRequests: helpRequestsFixtures.threeHelpRequests,
-    currentUser: currentUserFixtures.adminUser
-};
 

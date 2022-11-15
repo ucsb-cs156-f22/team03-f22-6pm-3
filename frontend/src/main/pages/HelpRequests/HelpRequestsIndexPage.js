a50b96a5fe8ae5454a7beced30c20175f1c6d204
Sergio Colis <sergioacolis@Sergios-MacBook-Pro.local>
2022-11-14 19:10:34 -0800

    SC - Replaced Placeholder Index Page

--- frontend/src/main/pages/HelpRequests/HelpRequestsIndexPage.js
+++ frontend/src/main/pages/HelpRequests/HelpRequestsIndexPage.js
@@ -1,13 +1,28 @@
 import React from 'react'
+import { useBackend } from 'main/utils/useBackend'; // use prefix indicates a React Hook
 
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import HelpRequestsTable from 'main/components/HelpRequests/HelpRequestTable';
+import { useCurrentUser } from 'main/utils/currentUser' // use prefix indicates a React Hook
 
 export default function HelpRequestsIndexPage() {
+
+  const currentUser = useCurrentUser();
+
+  const { data: requests, error: _error, status: _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      ["/api/helprequests/all"],
+            // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
+            { method: "GET", url: "/api/helprequests/all" },
+      []
+    );
+
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>Help Requests</h1>
-        <input type="text" placeholder="Help Requests placeholder text" />
+        <HelpRequestsTable requests={requests} currentUser={currentUser} />
       </div>
     </BasicLayout>
   )

f391f8b960b7660abd0758b18724dc03383ac94f
Jesus Velarde <jesus.velade07@gmail.com>
2022-11-15 16:12:57 -0800

    fix bug

--- frontend/src/tests/pages/MenuItemReview/MenuItemReviewsIndexPage.test.js
+++ frontend/src/tests/pages/MenuItemReview/MenuItemReviewsIndexPage.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { waitFor, render, test } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import MenuItemReviewsIndexPage from "main/pages/MenuItemReviews/MenuItemReviewsIndexPage";
